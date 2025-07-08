724b60419c505fa02ded620199f22b22513cf446
Robert Mikloš <r.miklos@email.cz>
2025-07-08 23:01:17 +0200

    Initial commit for mini-projects

---
+++ cofee-machine/main.py
@@ -0,0 +1,17 @@
+from source_data import MENU
+from source_data import resources
+
+# Základní nastavení
+
+# Funkce
+def report(data):
+    pass
+
+
+
+
+# Kód automatu
+user_choice = input("Co byste si dal/a (esspresso/latte/capuccino)")
+
+if user_choice == "report":
+    report(resources)
---
+++ cofee-machine/source_data.py
@@ -0,0 +1,32 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffe":18
+        },
+        "cost": 40
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffe":24
+        },
+        "cost": 50
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 150,
+            "milk": 100,
+            "coffe":24
+        },
+        "cost": 60
+    }
+}
+
+resources = {
+    "water": 400,
+    "milk": 300,
+    "coffe":150
+}
